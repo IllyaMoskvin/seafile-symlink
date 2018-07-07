IllyaMoskvin/seafile-symlink

commit 713d4f16132be2440715b1f03d172d996ac53e7f

Move ignore file read logic lower in the script

seafile-symlink.ps1
@@ -142,27 +142,6 @@ $rootPath = $Config['LibraryPath']
 # Extension to use for symlink placeholders
 $symExt = $Config['PlaceholderExt']
 
-# This is the separator b/w your manually ignored items, and auto-ignored symlinks
-$ignoreNeedle = '### SEAFILE-SYMLINK (AUTOGENERATED) ###'
-$ignoreWarning = '# Do not modify the line above or anything below it'
-
-# Split the ignore file into two parts based on our needle
-$siContent = Get-Content (Get-SeafileIgnoreFile $rootPath)
-$siPrefix = $siContent.Where({ $_ -Like $ignoreNeedle }, 'Until')
-
-# Create the suffix header
-$siSuffix = @($ignoreNeedle, $ignoreWarning)
-
-# Ensure that a newline precedes the suffix
-if (![string]::IsNullOrEmpty($siPrefix[-1])) {
-    $siPrefix += ''
-}
-
-# For comparison's sake, do the same to the source
-if (![string]::IsNullOrEmpty($siContent[-1])) {
-    $siContent += ''
-}
-
 # Look for symlink placeholder files, and create symlinks from them
 $phPaths = Get-SymbolicPlaceholders $rootPath
 
@@ -188,6 +167,7 @@ foreach ($phPath in $phPaths) {
 }
 
 $linkPaths = Get-SymbolicLinks $rootPath
+$linkIgnorePaths = @()
 
 foreach ($linkPath in $linkPaths) {
 
@@ -221,7 +201,7 @@ foreach ($linkPath in $linkPaths) {
         $linkIgnorePath = $linkIgnorePath + '/'
     }
 
-    $siSuffix += $linkIgnorePath
+    $linkIgnorePaths += $linkIgnorePath
 
     # If the path falls below the library root, keep it absolute, else make it relative
     # TODO: Make this a setting? Esp. how to treat paths on the same drive?
@@ -237,6 +217,27 @@ foreach ($linkPath in $linkPaths) {
 
     Write-Host "Created placeholder: `"$phFile`" >>> `"$destPath`""
 
+}
+
+# This is the separator b/w your manually ignored items, and auto-ignored symlinks
+$ignoreNeedle = '### SEAFILE-SYMLINK (AUTOGENERATED) ###'
+$ignoreWarning = '# Do not modify the line above or anything below it'
+
+# Split the ignore file into two parts based on our needle
+$siContent = Get-Content (Get-SeafileIgnoreFile $rootPath)
+$siPrefix = $siContent.Where({ $_ -Like $ignoreNeedle }, 'Until')
+
+# Create the suffix header
+$siSuffix = @($ignoreNeedle, $ignoreWarning)
+
+# Ensure that a newline precedes the suffix
+if (![string]::IsNullOrEmpty($siPrefix[-1])) {
+    $siPrefix += ''
+}
+
+# For comparison's sake, do the same to the source
+if (![string]::IsNullOrEmpty($siContent[-1])) {
+    $siContent += ''
 }
 
 # Add suffix to prefix with trailing newline

commit 74d633423548473b5cfd9e0a3249e7ad02c9e732

Fix appending of ignore paths to output
Regression in 713d4f16132be2440715b1f03d172d996ac53e7f

seafile-symlink.ps1
@@ -230,6 +230,9 @@ $siPrefix = $siContent.Where({ $_ -Like $ignoreNeedle }, 'Until')
 # Create the suffix header
 $siSuffix = @($ignoreNeedle, $ignoreWarning)
 
+# Append the ignore paths to our suffix
+$siSuffix += $linkIgnorePaths
+
 # Ensure that a newline precedes the suffix
 if (![string]::IsNullOrEmpty($siPrefix[-1])) {
     $siPrefix += ''