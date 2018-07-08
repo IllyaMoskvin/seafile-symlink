ed657222a18a4a5856872733ed9dbb1114b788f2
Illya Moskvin <imoskvin@artic.edu>
2018-07-07 23:44:09 -0500

    Move functions to top of script

--- seafile-symlink.sh
+++ seafile-symlink.sh
@@ -11,6 +11,26 @@ function error_exit {
     kill -s TERM $TOP_PID
 }
 
+# Retrieves a value from an ini file.
+# Trims trailing comments, spaces, and unwraps single quotes.
+function get_config_value {
+    # https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
+    v="$(awk -F "=" '/'$1'/ {print $2}' "$PRESET" | cut -f1 -d";" | tr -d ' ' | tr -d "'")"
+
+    if [ -z "$v" ]; then
+        error_exit "Missing key in config: $1"
+    fi
+
+    echo "$v"
+}
+
+# Convert potentially Windows path to Unix
+# https://stackoverflow.com/questions/13701218/windows-path-to-posix-path-conversion-in-bash
+function get_localized_path {
+    echo "$1" | sed -E 's/^([A-Za-z]):/\\\1/' | sed 's/\\/\//g'
+}
+
+
 # Ensure a preset name was passed
 if [ $# -ne 1 ]; then
     echo "Usage: $0 [prefixname]"
@@ -34,32 +54,12 @@ DIR_INIT="$(pwd)"
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# Retrieves a value from an ini file.
-# Trims trailing comments, spaces, and unwraps single quotes.
-function get_config_value {
-    # https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
-    v="$(awk -F "=" '/'$1'/ {print $2}' "$PRESET" | cut -f1 -d";" | tr -d ' ' | tr -d "'")"
-
-    if [ -z "$v" ]; then
-        error_exit "Missing key in config: $1"
-    fi
-
-    echo "$v"
-}
-
 # Unfortunately, associative arrays are only supported in bash 4+
 # Installing it on macOS is a hassle, so we'll stick with bash 3
 # https://apple.stackexchange.com/questions/193411/update-bash-to-version-4-0-on-osx
 LibraryPath="$(get_config_value 'LibraryPath')"
 StorageMethod="$(get_config_value 'StorageMethod')"
 PlaceholderExt="$(get_config_value 'PlaceholderExt')"
-
-
-# Convert potentially Windows path to Unix
-# https://stackoverflow.com/questions/13701218/windows-path-to-posix-path-conversion-in-bash
-function get_localized_path {
-    echo "$1" | sed -E 's/^([A-Za-z]):/\\\1/' | sed 's/\\/\//g'
-}
 
 # LibraryPath might be in Windows syntax.
 LibraryPath="$(get_localized_path "$LibraryPath")"
