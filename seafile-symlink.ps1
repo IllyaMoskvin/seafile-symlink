IllyaMoskvin/seafile-symlink

commit d53b0cb70517acf6ae9f423329bb434c5259fd47

PS: Update ignore file before creating symlinks
This is already the default behavior in the bash version.

seafile-symlink.ps1
@@ -588,9 +588,6 @@ $Data = Get-Data $Config['LibraryPath'] $Config['PlaceholderExt']
 # For debug, try uncommenting this before it changes data:
 # $Data | ForEach-Object { Write-Host @_ }; exit
 
-# Create actual symlinks from data
-$Data | ForEach-Object { New-SymbolicLink @_ $Config['LibraryPath'] }
-
 # Persist symlink data for syncing using specified StorageMethod
 Write-Host 'Using StorageMethod:' $Config['StorageMethod']
 
@@ -612,3 +609,7 @@ $IgnorePaths = $Data | ForEach-Object {
 
 # Write symlink paths to ignore file
 Write-SeafileIgnoreFile $Config['LibraryPath'] $IgnorePaths
+
+# Create actual symlinks from data
+$Data | ForEach-Object { New-SymbolicLink @_ $Config['LibraryPath'] }
+

commit 7a9da794f7daa32d828970e2730dd29901491d2e

PS: Handle non-existent `$Path` in `Get-RelativePath`
Fixes an error caused by d53b0cb70517acf6ae9f423329bb434c5259fd47,
wherein there are no symlinks to target when writing ignore file,
since they haven't been created yet. Only occurs on first-run.

seafile-symlink.ps1
@@ -186,11 +186,17 @@ function Assert-IsDirectory ([string]$DirPath, [string]$Param) {
 
 
 # Helper function for retrieving one path relative to another
+# Calls Resolve-Path but works for files that don't exist.
+# https://stackoverflow.com/questions/3038337/powershell-resolve-path-that-might-not-exist
 function Get-RelativePath ([string]$Path, [string]$DirPath) {
     Assert-IsDirectory $DirPath 'DirPath'
     Push-Location -Path $DirPath
-    $out = Resolve-Path -Relative $Path
+    $out = Resolve-Path $Path -Relative -ErrorAction 'SilentlyContinue' -ErrorVariable '_frperror'
+    if (-not($out)) {
+        $out = $_frperror[0].TargetObject
+    }
     Pop-Location
+
     $out
 }
 