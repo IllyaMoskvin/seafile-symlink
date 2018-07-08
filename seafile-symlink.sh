9e3567dea158d7132a8a67e363ea4f29b8067a6e
Illya Moskvin <imoskvin@artic.edu>
2018-07-07 22:57:55 -0500

    Accept preset argument

--- seafile-symlink.sh
+++ seafile-symlink.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+# Ensure a preset name was passed
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [prefixname]"
+    exit 1
+fi
+
+# Load the preset name from the argument
+PRESET=$1
+
+# If preset isn't an absolute path, look for an ini in `presets` subdir
+if [[ "$PRESET" != /* ]]; then
+   PRESET="${PRESET%.ini}.ini"
+   PRESET="presets/$PRESET"
+fi
+
+# Save the path from which this script was called. We'll restore it later.
+DIR_INIT="$(pwd)"
+
+# Change path to where the current script is located
+# We will need this for resolving relative paths within ini files
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 # Retrieves a value from an ini file.
 # Trims trailing comments, spaces, and unwraps single quotes.
 # https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
 function get_config_value {
-	awk -F "=" '/'$1'/ {print $2}' presets/default.ini | cut -f1 -d";" | tr -d ' ' | tr -d "'"
+    awk -F "=" '/'$1'/ {print $2}' "$PRESET" | cut -f1 -d";" | tr -d ' ' | tr -d "'"
 }
 
 # Unfortunately, associative arrays are only supported in bash 4+
@@ -18,4 +41,5 @@ echo $LibraryPath
 echo $StorageMethod
 echo $PlaceholderExt
 
-exit
+# Restore our initial working directory
+cd "$DIR_INIT"
