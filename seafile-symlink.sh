a11c3d7634182c01e0a5b38c5800af22aae7e746
Illya Moskvin <imoskvin@artic.edu>
2018-07-07 22:36:43 -0500

    Add ability to read ini files
    Obviously not robust. Handles our syntax just fine, though.

--- seafile-symlink.sh
+++ seafile-symlink.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# Retrieves a value from an ini file.
+# Trims trailing comments, spaces, and unwraps single quotes.
+# https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
+function get_config_value {
+	awk -F "=" '/'$1'/ {print $2}' presets/default.ini | cut -f1 -d";" | tr -d ' ' | tr -d "'"
+}
+
+# Unfortunately, associative arrays are only supported in bash 4+
+# Installing it on macOS is a hassle, so we'll stick with bash 3
+# https://apple.stackexchange.com/questions/193411/update-bash-to-version-4-0-on-osx
+LibraryPath=$(get_config_value 'LibraryPath')
+StorageMethod=$(get_config_value 'StorageMethod')
+PlaceholderExt=$(get_config_value 'PlaceholderExt')
+
+echo $LibraryPath
+echo $StorageMethod
+echo $PlaceholderExt
+
+exit
