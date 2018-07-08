f628777367e7ef0c8fd57290cef87687b14da275
Illya Moskvin <imoskvin@artic.edu>
2018-07-07 23:31:06 -0500

    Check for missing config keys

--- seafile-symlink.sh
+++ seafile-symlink.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Functions for trapping process and exiting from functions and sub-shells.
+# https://stackoverflow.com/questions/9893667/is-there-a-way-to-write-a-bash-function-which-aborts-the-whole-execution-no-mat
+# https://stackoverflow.com/questions/24597818/exit-with-error-message-in-bash-oneline
+trap "exit 1" TERM
+export TOP_PID=$$
+
+function error_exit {
+    echo "$1" >&2
+    kill -s TERM $TOP_PID
+}
+
 # Ensure a preset name was passed
 if [ $# -ne 1 ]; then
     echo "Usage: $0 [prefixname]"
@@ -25,17 +36,24 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # Retrieves a value from an ini file.
 # Trims trailing comments, spaces, and unwraps single quotes.
-# https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
 function get_config_value {
-    awk -F "=" '/'$1'/ {print $2}' "$PRESET" | cut -f1 -d";" | tr -d ' ' | tr -d "'"
+    # https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
+    v="$(awk -F "=" '/'$1'/ {print $2}' "$PRESET" | cut -f1 -d";" | tr -d ' ' | tr -d "'")"
+
+    if [ -z "$v" ]; then
+        error_exit "Missing key in config: $1"
+    fi
+
+    echo "$v"
 }
 
 # Unfortunately, associative arrays are only supported in bash 4+
 # Installing it on macOS is a hassle, so we'll stick with bash 3
 # https://apple.stackexchange.com/questions/193411/update-bash-to-version-4-0-on-osx
-LibraryPath=$(get_config_value 'LibraryPath')
-StorageMethod=$(get_config_value 'StorageMethod')
-PlaceholderExt=$(get_config_value 'PlaceholderExt')
+LibraryPath="$(get_config_value 'LibraryPath')"
+StorageMethod="$(get_config_value 'StorageMethod')"
+PlaceholderExt="$(get_config_value 'PlaceholderExt')"
+
 
 # Convert potentially Windows path to Unix
 # https://stackoverflow.com/questions/13701218/windows-path-to-posix-path-conversion-in-bash
